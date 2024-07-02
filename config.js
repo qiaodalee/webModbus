1b25f303d9fd21d38fa4fc9518e1546b0f2043ef
qiaodalee <u10906218@ms.ttu.edu.tw>
2024-07-02 12:14:58 +0800

    <Create> Config file

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+	port : '5020',
+	ip : '127.0.0.1',
+	host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database:'device',
+    dbport: 3306,
+    homeMasterIP: '10.103.151.2:1880'
+};
+
+export default config;
